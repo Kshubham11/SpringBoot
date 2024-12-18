2c53ae705b5393d11a45cf8e79fee2163fc30450
ShubhamKumar <shubhamsbhm@gmail.com>
2024-12-18 16:41:33 +0530

    committed

---
+++ src/main/java/com/sparrow11/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,22 @@
+package com.sparrow11.accounts.mapper;
+
+import com.sparrow11.accounts.dto.CustomerDto;
+import com.sparrow11.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+
+}
